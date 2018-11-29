408141076ce6b2b92242aae41e5cdb5fbbf3719d
Plum Ertz <plum.ertz@buzzfeed.com>
2018-11-29 06:01:39 -0500

    I'm not important

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,80 +3,89 @@
 var Promise = require('es6-promise').Promise;
 
 global.window = {
-  document: {},
-  Function: function(){}
+    document: {},
+    Function: function(){}
 };
 
 module.exports = function(grunt) {
-  grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
-    sass: {
-      lib: {
-        options: {
-          sourceMap: false,
-          includePaths: ['_lib']
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        sass: {
+            lib: {
+                options: {
+                    sourceMap: false,
+                    includePaths: ['_lib']
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: "_lib/",
+                        src: ['solid.scss'],
+                        dest: '.tmp',
+                        ext: '.<%= pkg.version %>.css'
+                    }
+                ]
+            }
         },
-        files: [
-          {
-            expand: true,
-            cwd: "_lib/",
-            src: ['solid.scss'],
-            dest: 'dist',
-            ext: '.<%= pkg.version %>.css'
-          }
+        group_css_media_queries: {
+            lib: {
+                options: {},
+                files: {
+                    'dist/solid.<%= pkg.version %>.css' : '.tmp/solid.<%= pkg.version %>.css'
+                }
+            }
+        },
+        cssnano: {
+            options: {
+                sourcemap: false
+            },
+            lib: {
+                files: {
+                    'dist/solid.<%= pkg.version %>.min.css' : 'dist/solid.<%= pkg.version %>.css'
+                }
+            }
+        },
+        compress: {
+            lib: {
+                options: {
+                    archive: 'dist/solid.<%= pkg.version %>.zip',
+                    mode: 'zip'
+                },
+                files: [{
+                    src: ['**/*'],
+                    cwd: '_lib/',
+                    expand: true
+                }]
+            }
+        },
+        copy: {
+            latest: {
+                files: [{
+                    expand: true,
+                    cwd: 'dist/',
+                    src: 'solid.<%= pkg.version %>.css',
+                    dest: 'dist/',
+                    rename: function(dest, src) {
+                        src = "solid.latest.css" //always available latest css
+                        return dest + src;
+                    }
+                }]
+            }
+        },
+        clean: [
+            'dist/',
+            '.tmp/'
         ]
-      }
-    },
-    cssnano: {
-      options: {
-        sourcemap: false
-      },
-      lib: {
-        files: {
-         'dist/solid.<%= pkg.version %>.min.css' : 'dist/solid.<%= pkg.version %>.css'
-        }
-      }
-    },
-    compress: {
-      lib: {
-         options: {
-             archive: 'dist/solid.<%= pkg.version %>.zip',
-             mode: 'zip'
-         },
-         files: [{
-             src: ['**/*'],
-             cwd: '_lib/',
-             expand: true
-         }]
-      }
-    },
-    copy: {
-      latest: {
-        files: [{
-          expand: true,
-          cwd: 'dist/',
-          src: 'solid.<%= pkg.version %>.css',
-          dest: 'dist/',
-          rename: function(dest, src) {
-            src = "solid.latest.css" //always available latest css
-            return dest + src;
-          }
-        }]
-      }
-    },
-    clean: [
-      'dist/',
-      '.tmp/'
-    ]
-  });
+    });
 
-grunt.loadNpmTasks('grunt-sass');
-grunt.loadNpmTasks('grunt-cssnano');
-grunt.loadNpmTasks('grunt-contrib-compress');
-grunt.loadNpmTasks('grunt-contrib-clean');
-grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-cssnano');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-group-css-media-queries');
 
-grunt.registerTask('prepare_solid_dist', ['sass:lib', 'cssnano:lib', 'copy:latest', 'compress:lib']);
-grunt.registerTask('dist', ['clean', 'prepare_solid_dist']);
+    grunt.registerTask('prepare_solid_dist', ['sass:lib', 'group_css_media_queries:lib', 'cssnano:lib', 'copy:latest', 'compress:lib']);
+    grunt.registerTask('dist', ['clean', 'prepare_solid_dist']);
 
 }
